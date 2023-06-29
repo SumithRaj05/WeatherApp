89624eea2c58ddd7128e8391990e6d3f8f5b6dfd
SumithRaj05 <sumithrajpurohit03+1@gmail.com>
2023-06-29 19:09:11 +0530

    updated

--- src/Components/API/WeatherApi.js
+++ src/Components/API/WeatherApi.js
@@ -23,8 +23,7 @@ const FetchWeather = (props) => {
 
     useEffect(() => {
         if (lat !== null && long !== null) {
-            const url = `http://api.weatherapi.com/v1/current.json?key=${import.meta.env.VITE_API_KEY}&q=${lat},${long}`;
-            console.log(import.meta.env)
+            const url = `https://api.weatherapi.com/v1/current.json?key=${import.meta.env.VITE_API_KEY}&q=${lat},${long}`;
             fetch(url)
                 .then((res) => res.json())
                 .then((data) => {
